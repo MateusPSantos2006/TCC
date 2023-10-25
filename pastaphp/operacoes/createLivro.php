ab7ae676a8d32ccac185dc1b31e8bfc1a40e07e3
Mateus Pinheiro dos Santos <mateuspinheiro2006@gmail.com>
2023-10-25 15:32:04 -0300

    hbujgess

--- pastaphp/operacoes/createLivro.php
+++ pastaphp/operacoes/createLivro.php
@@ -1,11 +1,11 @@
 <?php   
     require_once "../../vendor/autoload.php";
 
-    print_r(scandir("./"));
+    
     use TCC\validacoes\Livro as meuLivro;
     use TCC\banco\crud\Inserir as bota;
 
-    /*if(!isset($_POST['sinopse'])) exit("vai toma no cu racker!");
+        
     try {
         $livroForm = new meuLivro($_POST, $_FILES['capa']);
         $dadosLivro = $livroForm->getArrayLivro();
@@ -16,4 +16,4 @@
         exit();
     } catch (\Exception $maria) {
         echo $maria->getMessage();
-    }*/
+    }
