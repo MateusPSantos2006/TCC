02900f09ea68e8cfeab0c507fd5ac62591f456dc
Mateus Pinheiro dos Santos <mateuspinheiro2006@gmail.com>
2023-10-26 10:35:00 -0300

    tirando coisas desnecessariasa

--- pages/admin/cadastroLivros.php
+++ pages/admin/cadastroLivros.php
@@ -1,7 +1,3 @@
-<?php
-    require_once __DIR__ . "/../../vendor/autoload.php";
-?>
-
 <!doctype html>
 <html lang="pt-br">
   <head>
--- pages/admin/principalLogado.php
+++ pages/admin/principalLogado.php
@@ -1,7 +1,3 @@
-<?php
-    require_once "../../vendor/autoload.php";
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
