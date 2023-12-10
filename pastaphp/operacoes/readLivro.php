b8ede4a66283dd896e87a229a218034bcd97bcac
Mateus Pinheiro dos Santos <mateuspinheiro2006@gmail.com>
2023-12-10 17:17:29 -0300

    melhora das boas práticas

--- pastaphp/operacoes/readLivro.php
+++ pastaphp/operacoes/readLivro.php
@@ -12,10 +12,76 @@
             $dadosPesquisa = new Pesquisa($_GET);
     
             $retorno = new Ler;
-            $retorno->explorarProcura($dadosPesquisa->getTipoPesquisa(), $dadosPesquisa->getChavePesquisa());
+            $leitura = $retorno->explorarProcura($dadosPesquisa->getTipoPesquisa(), $dadosPesquisa->getChavePesquisa());
+
+            if ($leitura[1] == true) {
+                foreach ($leitura[0] as $objeto) {
+                    ?>
+                    <div class='cardResul'>
+                    <img src='../../imagens/capas/<?=$objeto['capa']?>' class="capa" alt='' >
+                        <article class='infosCardResul'>
+                            <div class='infosCardDados'>
+                                <p> <span class='enfase'>Título:</span><?=$objeto['titulo']?></p>
+                                <p><span class='enfase'>Autor:</span><?=$objeto['autor']?></p>
+                                <p> <span class='enfase'>Genero:</span><?=$objeto['genero']?></p>
+                                <p> <span class='enfase'>Nº de páginas:</span><?=$objeto['npags']?></p>
+                                <p> <span class='enfase'>Editora:</span><?=$objeto['editora']?></p>
+                                <p> <span class='enfase'>Status:</span><img class='simboloDisponibilidade' src='../../imagens/<?=$objeto['estado']?>.png'><?= $objeto['estado']?></p>
+                            </div>
+                            <p class='sinopse'> <span class='enfase'>Sinópse:</span><?=$objeto['sinopse']?></p>
+                        </article>
+                    </div>
+                    <?php
+                }
+            }else{
+                ?>
+                <div class='alerta'>
+                    <p>
+                        Nenhum livro encontrado com os dados pesquisados, pesquisa geral feita
+                    </p>
+                </div>
+                <?php
+                foreach ($leitura[0] as $objeto) {
+                    ?>
+                    <div class='cardResul'>
+                    <img src='../../imagens/capas/<?=$objeto['capa']?>' class="capa" alt='' >
+                        <article class='infosCardResul'>
+                            <div class='infosCardDados'>
+                                <p> <span class='enfase'>Título:</span><?=$objeto['titulo']?></p>
+                                <p><span class='enfase'>Autor:</span><?=$objeto['autor']?></p>
+                                <p> <span class='enfase'>Genero:</span><?=$objeto['genero']?></p>
+                                <p> <span class='enfase'>Nº de páginas:</span><?=$objeto['npags']?></p>
+                                <p> <span class='enfase'>Editora:</span><?=$objeto['editora']?></p>
+                                <p> <span class='enfase'>Status:</span><img class='simboloDisponibilidade' src='../../imagens/<?=$objeto['estado']?>.png'><?= $objeto['estado']?></p>
+                            </div>
+                            <p class='sinopse'> <span class='enfase'>Sinópse:</span><?=$objeto['sinopse']?></p>
+                        </article>
+                    </div>
+                    <?php
+                }
+            }
         }else{
             $retorno = new Ler;
-            $retorno->explorarTudo();
+            $leitura = $retorno->explorarTudo();
+
+            foreach ($leitura as $objeto) {
+                ?>
+                <div class='cardResul'>
+                    <img src='../../imagens/capas/<?=$objeto['capa']?>' class="capa" alt='' >
+                    <article class='infosCardResul'>
+                        <div class='infosCardDados'>
+                            <p> <span class='enfase'>Título:</span><?=$objeto['titulo']?></p>
+                            <p><span class='enfase'>Autor:</span><?=$objeto['autor']?></p>
+                            <p> <span class='enfase'>Genero:</span><?=$objeto['genero']?></p>
+                            <p> <span class='enfase'>Nº de páginas:</span><?=$objeto['npags']?></p>
+                            <p> <span class='enfase'>Editora:</span><?=$objeto['editora']?></p>
+                            <p> <span class='enfase'>Status:</span><img class='simboloDisponibilidade' src='../../imagens/<?=$objeto['estado']?>.png'><?= $objeto['estado']?></p>
+                        </div>
+                        <p class='sinopse'> <span class='enfase'>Sinópse:</span><?=$objeto['sinopse']?></p>
+                    </article>
+                </div>
+                <?php
+            }
         }
     } catch (\Exception $erro) {
         echo $erro->getMessage();
--- pastaphp/operacoes/readLivroADM.php
+++ pastaphp/operacoes/readLivroADM.php
@@ -8,18 +8,102 @@
     use TCC\validacoes\Pesquisa;
 
     try {
-        $retorno = new Ler;
         if (isset($_GET['tipo']) && isset($_GET['dado'])) {
             $dadosPesquisa = new Pesquisa($_GET);
     
-            $retorno->explorarProcuraADM($dadosPesquisa->getTipoPesquisa(), $dadosPesquisa->getChavePesquisa());
-        }else{
-            $retorno->explorarTudoADM();
-        }
+            $retorno = new Ler;
+            $leitura = $retorno->explorarProcura($dadosPesquisa->getTipoPesquisa(), $dadosPesquisa->getChavePesquisa());
 
+            if ($leitura[1] == true) {
+                foreach ($leitura[0] as $objeto) {
+                    ?>
+                    <div class='livro'>
+                        <div class='cardResul' id='<?=$objeto['id']?>'>
+                            <img src='../../imagens/capas/<?=$objeto['capa']?>' class="capa" alt='' >
+                            <article class='infosCardResul'>
+                                <div class='infosCardDados'>
+                                    <p> <span class='enfase'>Título: </span><?=$objeto['titulo']?></p>
+                                    <p><span class='enfase'>Autor: </span><?=$objeto['autor']?></p>
+                                    <p> <span class='enfase'>Genero: </span><?=$objeto['genero']?></p>
+                                    <p> <span class='enfase'>Nº de páginas: </span><?=$objeto['npags']?></p>
+                                    <p> <span class='enfase'>Editora: </span><?=$objeto['editora']?></p>
+                                    <p> <span class='enfase'>Status: </span> <img class='simboloDisponibilidade' src='../../imagens/<?=$objeto['estado']?>.png'><?=$objeto['estado']?></p>
+                                </div>
+                                <p class='sinopse'> <span class='enfase'>Sinópse: </span><?=$objeto['sinopse']?></p>
+                            </article>
+                        </div>
+                        <div class='botoes'>
+                            <button type='button' class='btn btn-primary update' data-valor='<?=$objeto['id']?>'>modificar</button>
+                            <button type='button' class='btn btn-danger apagar' data-valor='<?=$objeto['id']?>'>apagar</button>
+                            <button type="button" class="btn btn-primary recomendar" data-valor='<?=$objeto['id']?>'>recomendar</button>
+                        </div>
+                    </div>
+                    <?php
+                }
+            }else{
+                ?>
+                <div class='alerta'>
+                    <p>
+                        Nenhum livro encontrado com os dados pesquisados, pesquisa geral feita
+                    </p>
+                </div>
+                <?php
+                foreach ($leitura[0] as $objeto) {
+                    ?>
+                    <div class='livro'>
+                        <div class='cardResul' id='<?=$objeto['id']?>'>
+                            <img src='../../imagens/capas/<?=$objeto['capa']?>' class="capa" alt='' >
+                            <article class='infosCardResul'>
+                                <div class='infosCardDados'>
+                                    <p> <span class='enfase'>Título: </span><?=$objeto['titulo']?></p>
+                                    <p><span class='enfase'>Autor: </span><?=$objeto['autor']?></p>
+                                    <p> <span class='enfase'>Genero: </span><?=$objeto['genero']?></p>
+                                    <p> <span class='enfase'>Nº de páginas: </span><?=$objeto['npags']?></p>
+                                    <p> <span class='enfase'>Editora: </span><?=$objeto['editora']?></p>
+                                    <p> <span class='enfase'>Status: </span> <img class='simboloDisponibilidade' src='../../imagens/<?=$objeto['estado']?>.png'><?=$objeto['estado']?></p>
+                                </div>
+                                <p class='sinopse'> <span class='enfase'>Sinópse: </span><?=$objeto['sinopse']?></p>
+                            </article>
+                        </div>
+                        <div class='botoes'>
+                            <button type='button' class='btn btn-primary update' data-valor='<?=$objeto['id']?>'>modificar</button>
+                            <button type='button' class='btn btn-danger apagar' data-valor='<?=$objeto['id']?>'>apagar</button>
+                            <button type="button" class="btn btn-primary recomendar" data-valor='<?=$objeto['id']?>'>recomendar</button>
+                        </div>
+                    </div>
+                    <?php
+                }
+            }
+        }else{
+            $retorno = new Ler;
+            $leitura = $retorno->explorarTudo();
+
+            foreach ($leitura as $objeto) {
+                ?>
+                <div class='livro'>
+                    <div class='cardResul' id='<?=$objeto['id']?>'>
+                        <img src='../../imagens/capas/<?=$objeto['capa']?>' class="capa" alt='' >
+                        <article class='infosCardResul'>
+                            <div class='infosCardDados'>
+                                <p> <span class='enfase'>Título: </span><?=$objeto['titulo']?></p>
+                                <p><span class='enfase'>Autor: </span><?=$objeto['autor']?></p>
+                                <p> <span class='enfase'>Genero: </span><?=$objeto['genero']?></p>
+                                <p> <span class='enfase'>Nº de páginas: </span><?=$objeto['npags']?></p>
+                                <p> <span class='enfase'>Editora: </span><?=$objeto['editora']?></p>
+                                <p> <span class='enfase'>Status: </span> <img class='simboloDisponibilidade' src='../../imagens/<?=$objeto['estado']?>.png'><?=$objeto['estado']?></p>
+                            </div>
+                            <p class='sinopse'> <span class='enfase'>Sinópse: </span><?=$objeto['sinopse']?></p>
+                        </article>
+                    </div>
+                    <div class='botoes'>
+                        <button type='button' class='btn btn-primary update' data-valor='<?=$objeto['id']?>'>modificar</button>
+                        <button type='button' class='btn btn-danger apagar' data-valor='<?=$objeto['id']?>'>apagar</button>
+                        <button type="button" class="btn btn-primary recomendar" data-valor='<?=$objeto['id']?>'>recomendar</button>
+                    </div>
+                </div>
+                <?php
+            }
+        }
     } catch (\Exception $erro) {
         echo $erro->getMessage();
     }
-
-
-    
