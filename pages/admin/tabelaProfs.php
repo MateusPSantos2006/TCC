df8f008e7192a16e75fd31979cdae41f9112e938
Mateus Pinheiro dos Santos <mateuspinheiro2006@gmail.com>
2023-12-10 02:20:54 -0300

    vlibras colocado

--- pages/admin/tabelaProfs.php
+++ pages/admin/tabelaProfs.php
@@ -20,6 +20,16 @@
             require_once "../../pastaphp/operacoes/verificarCookie.php";
         ?>
         <header>
+        <div vw class="enabled">
+    <div vw-access-button class="active"></div>
+    <div vw-plugin-wrapper>
+      <div class="vw-plugin-top-wrapper"></div>
+    </div>
+  </div>
+  <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+  <script>
+    new window.VLibras.Widget();
+  </script> 
             <a href="./principalLogado.php" class="linkHeader algoQueDeveServirComoLogo">
                 <img src="../../imagens/logo.png" class="logo">
                 <p>
