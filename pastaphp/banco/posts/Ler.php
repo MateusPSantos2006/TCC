f8fdef631827ca8ebb7fcc6d8d821d0bcb9f7ce2
Mateus Pinheiro dos Santos <mateuspinheiro2006@gmail.com>
2023-12-10 22:50:52 -0300

    paginação dos posts completa (testless)
    falta testar

--- pastaphp/banco/posts/Ler.php
+++ pastaphp/banco/posts/Ler.php
@@ -3,8 +3,11 @@
     use TCC\banco\ConexaoPdo;
 
     class Ler {
-        public function getDadosPost(){
+        public function getDadosPost($pagina){
             try{
+                $limite = 5;
+                $inicio = ($pagina * $limite) - $limite;
+
                 $db = new ConexaoPdo;
                 $db = $db->conectar();
     
@@ -12,7 +15,8 @@
                 FROM posts
                 INNER JOIN profs ON posts.idProf = profs.id
                 INNER JOIN livros ON posts.idLivro = livros.id
-                ORDER BY dataEnvio DESC;";
+                ORDER BY dataEnvio DESC
+                LIMIT $inicio, $limite;";
                 $dados = $db->query($sql);
                 $db=null;
     
@@ -20,5 +24,17 @@
             } catch (\Exception $erro) {
                 echo "Erro ao tentar ler post";
             }
+        }
+        function getNumeroTotal(){
+            $db = new ConexaoPdo;
+            $db = $db->conectar();
+    
+            $sql = "SELECT COUNT(id) numero FROM posts;";
+            $total = $db->query($sql)->fetch()["numero"];
+            $db=null;
+    
+            $total = ceil($total / 5);
+
+            return $total;
         }
     }
--- pastaphp/operacoes/readPostADM.php
+++ pastaphp/operacoes/readPostADM.php
@@ -6,8 +6,19 @@
     require_once "../../vendor/autoload.php";
     use TCC\banco\posts\Ler;
 
+    
     $leitura = new Ler;
-    $dados = $leitura -> getDadosPost();
+    $maximo = $leitura->getNumeroTotal();
+
+        if (isset($_GET["pagina"]) && $_GET["pagina"] != null){
+            $entrada = filter_input(INPUT_GET, "pagina", FILTER_SANITIZE_NUMBER_INT);
+            $entrada = $entrada != null ? $entrada : 1;
+            $entrada = $entrada >= 1 ? $entrada : 1;
+            $entrada = $entrada <= $maximo ? $entrada : $maximo;
+        }else{
+            $entrada = 1;
+        }
+    $dados = $leitura -> getDadosPost($entrada);
     $aux = 0;
     foreach ($dados as $algo) {
         ?>
@@ -27,7 +38,32 @@
         <?php
         $aux++;
     }
-    if($aux < 1) {
+    if ($aux>=1) {
+        ?>
+    </div>
+  <div id="botoesList">
+    <ul class="pagination">
+        <li class="page-item"><a class="page-link" href="?pagina=1">Primeira</a></li>
+        <?php
+            if ($entrada > 1) {
+                ?>
+                <li class="page-item"><a class="page-link" href="?pagina=<?=$entrada-1?>">Anterior</a></li>
+                <?php
+            }
+        ?>
+        <li class="page-item"><div class="page-link"><?=$entrada?></div></li>
+        <?php
+            if ($entrada < $maximo) {
+                ?>
+                <li class="page-item"><a class="page-link" href="?pagina=<?=$entrada+1?>">Próximo</a></li>
+                <?php
+            }
+        ?>
+        <li class="page-item"><a class="page-link" href="?pagina=<?=$maximo?>">Última</a></li>
+    </ul>
+  </div>
+  <?php
+    }else{
         ?>
             <div class="alerta">
                 <p>
